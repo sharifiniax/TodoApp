1245d8c2383a4c07cf5fceee6be9dfbdf08686f4
mksh <sharifiniax@gmail.com>
2022-09-06 23:44:45 +0430

    just because I can access project remotely.

--- app/src/main/java/org/sharifinia/todo/MainActivity.kt
+++ app/src/main/java/org/sharifinia/todo/MainActivity.kt
@@ -1,12 +1,13 @@
 package org.sharifinia.todo
 
+
 import android.os.Bundle
-import android.widget.CheckBox
-import android.widget.ImageButton
+import android.text.style.AlignmentSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,19 +17,23 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.sharifinia.todo.models.DayModel
 import org.sharifinia.todo.models.MonthModel
 import org.sharifinia.todo.models.Task
 import org.sharifinia.todo.models.TodoCategory
 import org.sharifinia.todo.ui.theme.Shapes
-
 import org.sharifinia.todo.ui.theme.TodoTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,21 +62,40 @@ fun CategoryList(
     modifier: Modifier = Modifier,
     categoryList: List<TodoCategory>
 ) {
-    LazyRow() {
+    LazyRow(modifier=Modifier.padding(8.dp)) {
         items(categoryList) {
             CategoryItem(item = it, modifier)
-
+            Spacer(modifier = Modifier.width(8.dp))
         }
     }
-    Spacer(modifier = Modifier.height(8.dp))
+    Spacer(modifier = Modifier.height(16.dp))
 }
 
 @Composable
 fun CategoryItem(item: TodoCategory, modifier: Modifier = Modifier) {
 
 
-    Column(modifier = Modifier.background(Color(item.color), shape = Shapes.small)) {
-        Text(text = item.name)
+    Column(
+        modifier = Modifier
+            .defaultMinSize(50.dp, 50.dp)
+            .background(Color(item.color), shape = Shapes.small)
+
+
+    ) {
+        Box(modifier= Modifier
+            .defaultMinSize(70.dp, 50.dp)
+            .clip(Shapes.small)
+            .background(Color(item.color))
+            .border(BorderStroke(1.dp, Color.Black), RectangleShape)
+            ,
+            contentAlignment = Alignment.Center,
+
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.Center
+            ) {
+            Text(text = item.name)
+        }
+
     }
 
 }
@@ -94,56 +118,61 @@ fun TodoList(
 
 @Composable
 fun TodoItem(item: Task, modifier: Modifier = Modifier) {
+
     val expand = remember {
         mutableStateOf(false)
     }
-    Column(modifier=Modifier.clickable {
-        expand.value=!expand.value
-    }) {
 
-        Row(modifier = Modifier) {
+    Row(
+        modifier = Modifier
 
-            Checkbox(checked = item.done, onCheckedChange = {})
-            Text(text = item.title)
-            Icon(
-                painter = painterResource(id = R.drawable.ic_baseline_delete_outline_24),
-                contentDescription = "delete item"
-            )
-
-
-        }
-
-        Row() {
-            if (expand.value) {
-                item.description?.let {
-                    Text(text = it)
-                }
+            .clickable {
+                expand.value = !expand.value
             }
-        }
-    }
-
-
-
-
-}
-
-
-@Composable
-fun ActionButton(modifier: Modifier = Modifier) {
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-
-            }) {
-
-            }
-        },
-        isFloatingActionButtonDocked = true,
-        //floatingActionButtonPosition = FabPosition.End
+            .padding(8.dp),
+        verticalAlignment = if (expand.value) Alignment.Top else Alignment.CenterVertically
 
     ) {
 
+        Checkbox(modifier = modifier
+            .weight(0.1F),
+            checked = item.done,
+            onCheckedChange = {}
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(
+            modifier = modifier
+                .weight(0.9F)
+        ) {
+            Text(text = item.title, fontWeight = FontWeight.Bold)
+
+            if (expand.value) {
+                Spacer(modifier = Modifier.height(16.dp))
+                item.description?.let {
+                    Text(text = it.repeat(33))
+                }
+            }
+        }
+        Icon(
+            modifier = modifier
+                .weight(0.1F)
+                .clickable { },
+            painter = painterResource(id = R.drawable.ic_baseline_edit_24),
+            contentDescription = "delete item"
+        )
+        Icon(
+            modifier = modifier
+                .weight(0.1F)
+                .clickable { },
+            painter = painterResource(id = R.drawable.ic_baseline_delete_outline_24),
+            contentDescription = "delete item"
+        )
+
+
     }
+
 
 }
 
@@ -153,28 +182,53 @@ fun ActionButton(modifier: Modifier = Modifier) {
 fun DefaultPreview() {
     val cats = mutableListOf<TodoCategory>()
     val tasks = mutableListOf<Task>()
-    for (c in 1..8){
+    for (c in 1..8) {
         cats.add(
-        TodoCategory(
-            name = "cat$c", 0xFFFFFFFF
-        )
+            TodoCategory(
+                name = "cat$c", color = 0xFFFAFFFF
+            )
         )
     }
-    for (t in 1..50){
+    for (t in 1..50) {
         tasks.add(
-        Task("title$t",
-            "Random$t",
-            day = DayModel(1*t,
-                2,11,2022,MonthModel.Current
+            Task(
+                "title$t",
+                "Random$t",
+                day = DayModel(
+                    1 * t,
+                    2, 11, 2022, MonthModel.Current
 
-                ),2
-            ))
+                ), 2
+            )
+        )
 
     }
 
 
     TodoTheme {
-        CategoryList(categoryList =cats )
-        TodoList(todoList = tasks)
+        Scaffold(
+            floatingActionButton = {
+
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Text(text = "+", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                }
+            }
+    
+
+
+        ) {
+
+
+            Column() {
+                Spacer(modifier = Modifier.height(16.dp))
+                CategoryList(categoryList = cats, modifier = Modifier.fillMaxWidth())
+                Spacer(modifier = Modifier.height(16.dp))
+                TodoList(todoList = tasks, modifier = Modifier.fillMaxWidth())
+
+            }
+
+
+        }
+
     }
 }
--- app/src/main/java/org/sharifinia/todo/ui/theme/Type.kt
+++ app/src/main/java/org/sharifinia/todo/ui/theme/Type.kt
@@ -3,6 +3,7 @@ package org.sharifinia.todo.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -13,6 +14,11 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
+//    h1 = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Bold,
+//        fontSize = 16.sp
+//    )
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
